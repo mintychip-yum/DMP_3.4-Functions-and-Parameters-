e980e86e9fd9eb93d992b2b8236fda5bbe84b6da
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-02-01 14:50:17 -0600

    updated feb1 2;50pm

---
+++ myScript.js
@@ -0,0 +1,30 @@
+const name= "its'a me mario";
+
+
+function toUpper(text) {
+    const upperCased = text.toUpperCase();
+    console.log (upperCased);
+     } 
+  toUpper(name);
+  
+
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "GET RICK ROLLED NERD! >:p";   
+    }
+
+
+
+    const img= document.createElement('img');
+    document.body.appendChild (img)
+    img.src = 'rick-roll-rick-rolled.gif'
+
+  
+
+    
+ 
+  
+
+
+
+
+ 
