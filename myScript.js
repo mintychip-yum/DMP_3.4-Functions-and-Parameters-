28ed82add0363776bc809eb0524ce9bb784555fa
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-02-01 15:48:22 -0600

    Updated index.html_myscript.js feb1 3:48pm

--- myScript.js
+++ myScript.js
@@ -1,6 +1,5 @@
 const name= "its'a me mario";
 
-
 function toUpper(text) {
     const upperCased = text.toUpperCase();
     console.log (upperCased);
@@ -8,20 +7,23 @@ function toUpper(text) {
   toUpper(name);
 //This is a invoked function
 
-    function myFunction() {
-        document.getElementById("demo").innerHTML = "GET RICK ROLLED NERD! >:p";  
-        document.getElementById("buttonclick").innerHTML = "YOU CLICKED ME!" 
-    }//Funtion will display string
-    //Ivoke image and text elements.
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "GET RICK ROLLED NERD! >:p";  
+    //document.getElementById("buttonclick").innerHTML = "YOU CLICKED ME!" 
+}//Funtion will display string
+//Ivoke image and text elements.
 
 
 
 
-    function myFunction(text) {
-        let text = document.getElementById("answer").innerHTML;
-        document.getElementById("secondmain").innerHTML =
-        text.replace("Who's a silly goose?","Youre a silly goose!");
-    }
+function myFunction(text) {
+    let text = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML =
+    text.replace("Who's a silly goose?","Youre a silly goose!");
+} //This is a javascript string method
+// When button is clicked text should be replaced to "Youre a silly goose!"
+console.log(document.getElementById("answer"));
 
 
 
