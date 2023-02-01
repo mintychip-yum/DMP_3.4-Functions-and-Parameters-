32bf0adc45661da839180d41c7a76e86d9f797e4
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-02-01 15:40:07 -0600

    Updated feb1 3:40pm

--- myScript.js
+++ myScript.js
@@ -6,23 +6,35 @@ function toUpper(text) {
     console.log (upperCased);
      } 
   toUpper(name);
-  
+//This is a invoked function
 
     function myFunction() {
-        document.getElementById("demo").innerHTML = "GET RICK ROLLED NERD! >:p";   
+        document.getElementById("demo").innerHTML = "GET RICK ROLLED NERD! >:p";  
+        document.getElementById("buttonclick").innerHTML = "YOU CLICKED ME!" 
+    }//Funtion will display string
+    //Ivoke image and text elements.
+
+
+
+
+    function myFunction(text) {
+        let text = document.getElementById("answer").innerHTML;
+        document.getElementById("secondmain").innerHTML =
+        text.replace("Who's a silly goose?","Youre a silly goose!");
     }
 
 
 
-    const img= document.createElement('img');
-    document.body.appendChild (img)
-    img.src = 'rick-roll-rick-rolled.gif'
-
-  
 
     
- 
-  
+const person= {
+    name: "Rick Roll",
+    age: 57,
+    ethnicity: "White",
+    occupation: "Singer"
+    hair color: "Ginger",
+}
+//Object w/ multiple properties
 
 
 
